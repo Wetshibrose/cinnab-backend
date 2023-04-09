709ff1371a526f75351551980d137aaaac9b2339
Jaydrey <jaribwetshi64@gmail.com>
2023-04-09 22:04:14 +0300

    feat(app urls): added the genders endpoints to user route

--- users/urls.py
+++ users/urls.py
@@ -2,16 +2,26 @@ from django.urls import path
 
 # views
 from .views import (
-    RetrieveUsersAPIView, RetrieveUserAPIView, CreateUserAPIView, EditUserAPIView, DeleteUserAPIView ,
+    RetrieveUsersAPIView, 
+    RetrieveUserAPIView, 
+    CreateUserAPIView, 
+    EditUserAPIView, 
+    DeleteUserAPIView, 
+    RetrieveGenderTypesAPIView,
+    RetrieveGenderTypeAPIView,
+    CreateGenderTypeAPIView
     )
 
 app_name = "users"
 
 urlpatterns = [
     path("", RetrieveUsersAPIView.as_view(), name="users",),
-    path("<str:user_id>", RetrieveUserAPIView.as_view(), name="user"),
     path("newUser", CreateUserAPIView.as_view(), name="new_user",),
+    path("<str:user_id>", RetrieveUserAPIView.as_view(), name="user"),
     path("editUser/<str:user_id>", EditUserAPIView.as_view(), name="edit_user",),
     path("deleteUser/<str:user_id>", DeleteUserAPIView.as_view(), name="delete_user",),
+    path("genders/", RetrieveGenderTypesAPIView.as_view(), name="gender_types"),
+    path("genders/newGender", CreateGenderTypeAPIView.as_view(), name="new_gender_type"),
+    path("genders/<str:gender_id>", RetrieveGenderTypeAPIView.as_view(), name="gender_type"),
 ]
 
