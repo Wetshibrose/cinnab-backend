21a285ce42c536fdd10e42adec718abd0494e06d
Jaydrey <jaribwetshi64@gmail.com>
2023-04-22 16:24:07 +0300

    feat(app models): added permissions, index, verbose meta params

--- theme/models.py
+++ theme/models.py
@@ -5,6 +5,21 @@ from django.utils import timezone
 
 
 class Theme(models.Model):
+    class Meta:
+        default_related_name = "themes"
+        indexes = [
+            models.Index(fields=["id"])
+        ]
+        ordering = ["-created_at"]
+        permissions = (
+            ("can_view_theme", "Can view theme"),
+            ("can_add_theme", "Can add theme"),
+            ("can_edit_theme", "Can edit theme"),
+            ("can_delete_theme", "Can delete theme"),
+        )
+        verbose_name = "theme"
+        verbose_name_plural = "themes"
+
     id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
     primary_color = models.CharField(max_length=200, default="", blank="")
     is_deleted = models.BooleanField(default=False)
